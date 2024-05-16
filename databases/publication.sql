37b2a4f124989d3b95744a0d0f16a18804a2a9b9
michaellin442 <michaellin442442@gmail.com>
2024-05-16 17:18:18 -0400

    project formatting

---
+++ databases/publication.sql
@@ -0,0 +1,13 @@
+CREATE TABLE publications (
+	id INTEGER,
+	publication_title VARCHAR(256),
+	authors VARCHAR(1024),
+	published_date VARCHAR(50),
+	journal VARCHAR(50),
+	doi VARCHAR(50),
+	study_id VARCHAR(50),
+	study_name VARCHAR(50),
+	PMID INTEGER,
+	link_url VARCHAR(50),
+	file_link VARCHAR(50)
+);

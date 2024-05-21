0dae26ddb9f06a310e8486516dd743d723ea4722
michaellin442 <michaellin442442@gmail.com>
2024-05-21 14:02:16 -0400

    add to database functions

--- databases/publication.sql
+++ databases/publication.sql
@@ -1,13 +1,13 @@
 CREATE TABLE publications (
 	publication_id INTEGER PRIMARY KEY AUTOINCREMENT,
-	publication_title VARCHAR(256),
+	publication_title VARCHAR(1000),
 	published_date VARCHAR(50),
-	journal VARCHAR(50),
+	journal VARCHAR(256),
 	doi VARCHAR(50),
 	study_id VARCHAR(50),
 	study_name VARCHAR(50),
 	PMID INTEGER,
-	link_url VARCHAR(50),
+	link_url VARCHAR(256),
 	file_link VARCHAR(50)
 );
 
@@ -16,14 +16,15 @@ CREATE TABLE all_contributors (
 	first_name VARCHAR(50),
 	last_name VARCHAR(50),
 	short_name VARCHAR(50),
-	organization VARCHAR(4)
+	organization VARCHAR(4),
+	is_associate VARCHAR (4),
+	is_refered VARCHAR (4)
 );
 
 CREATE TABLE authors (
 	short_name VARCHAR(50),
 	author_role VARCHAR(50),
-	id INTEGER,
-	FOREIGN KEY (id) REFERENCES publications(publication_id)
+	publication_id INTEGER REFERENCES publications(publication_id)
 );
 
 --DROP TABLE publications
--- src/init.py
+++ src/init.py
@@ -1,17 +1,99 @@
 import pandas as pd 
+import sqlite3
+import re
 
-#non-PHRI associates
+
+#add data to database functions
+
+db = sqlite3.connect("databases/publications.db")
+
+def add_author(conn, contributor):
+    sql = '''INSERT INTO all_contributors(full_name,first_name,last_name,short_name,organization,is_associate,is_refered)
+             VALUES(?,?,?,?,?,?,?)'''
+    cur = db.cursor()
+    cur.execute(sql, contributor)
+    db.commit()
+    return cur.lastrowid
+
+def add_publication(conn, publications):
+    sql = '''INSERT INTO publications(publication_id,publication_title,published_date,journal,doi,study_id,study_name,PMID,link_url,file_link)
+             VALUES(?,?,?,?,?,?,?,?,?,?)'''
+    cur = db.cursor()
+    cur.execute(sql, publications)
+    db.commit()
+    return cur.lastrowid
+
+def add_publication_author(conn, authors):  
+    sql = '''INSERT INTO authors(short_name,author_role,publication_id)
+             VALUES(?,?,?)'''
+    cur = db.cursor()
+    cur.execute(sql, authors)
+    db.commit()
+    return cur.lastrowid
+
+#import non-PHRI associates excel data to database
 print("\nNon-PHRI Associates\n----------------------------------")
-authors_dataframe = pd.read_excel("input_files/PHRI Authors List.xlsx",sheet_name = "non - Associate", usecols=(0,1), header = None) 
-print(authors_dataframe)
+authors_dataframe = pd.read_excel("input_files/PHRI Authors List.xlsx", header=None, usecols=[0,1], sheet_name="non - Associate", na_filter=False)
+for row in authors_dataframe.itertuples(index=None):
+    names = list(row)
+    short_name = names[1]
+    full_name = names[0]
+    long_name = (names[0]).split(',')
+    if len(long_name) > 1:
+        last_name = long_name[0]
+        first_name = long_name[1]
+    else:
+        long_name = names[1].split()
+        last_name = long_name[0]
+        first_name = long_name[1]
 
-#PHRI associates
+    contributor = (full_name, first_name, last_name, short_name,"","True", "True")
+
+#import PHRI associates excel data to database
 print("\nPHRI Associates\n----------------------------------")
 PHRI_authors_dataframe = pd.read_excel("input_files/PHRI Authors List.xlsx",sheet_name = "Associate", usecols=(0,2), header = None) 
-print(PHRI_authors_dataframe)
+for row in authors_dataframe.itertuples(index=None):
+    names = list(row)
+    short_name = names[1]
+    full_name = names[0]
+    long_name = (names[0]).split(',')
+    if len(long_name) > 1:
+        last_name = long_name[0]
+        first_name = long_name[1].strip()
+    else:
+        long_name = names[1].split()
+        last_name = long_name[0]
+        first_name = long_name[1].strip()
 
-#publications
+    contributor = (full_name, first_name, last_name, short_name,"PHRI","True", "True")
+    add_author(db, contributor)
+
+#import publications data to database
 print("\nPublications\n----------------------------------")
 publications_dataframe = pd.read_csv("input_files/publications.csv") 
-print(publications_dataframe)
+for row in publications_dataframe.itertuples(index=None):
+    values = list(row)
+    publication_id = values[0]
+    publication_title = values[1]
+    published_date = values[3]
+    journal = values[4]
+    doi = values[5]
+    study_id = values[6]
+    study_name = values[7]
+    PMID = values[8]
+    link_url = values[9]
+    file_link = values[10]
+    publication = (publication_id,publication_title,published_date,journal,doi,study_id,study_name,PMID,link_url,file_link)
+    add_publication(db, publication)
 
+    pub_authors = re.split(r',|;', values[2])
+    for i in pub_authors:
+        short_name = i.strip()
+        if pub_authors.index(i) == 0:
+            author_role = "First Listed Author"
+        elif pub_authors.index(i) == (len(pub_authors)-1):
+            author_role = "Last Author"
+        else: 
+            author_role = "Co-Author"
+        authors = (short_name,author_role,publication_id)
+        add_publication_author(db, authors)
